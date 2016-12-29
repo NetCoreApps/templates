49565979b0471be4e28eacd759797c8c486e658d
Darren Reid <darren@solcast.com.au>
2016-12-29 14:30:24 +1100

    First pass at default core template files.

---
+++ src/CoreEmpty/Tests/Tests.cs
@@ -0,0 +1,41 @@
+﻿using $saferootprojectname$.ServiceInterface;
+using $saferootprojectname$.ServiceModel;
+using NUnit.Framework;
+using ServiceStack;
+using ServiceStack.Testing;
+
+namespace $saferootprojectname$.Tests
+{
+    public class UnitTests
+    {
+        private readonly ServiceStackHost appHost;
+
+        public UnitTests()
+        {
+            appHost = new BasicAppHost(typeof(MyServices).GetAssembly())
+                {
+                    ConfigureContainer = container =>
+                    {
+                        //Add your IoC dependencies here
+                    }
+                }
+                .Init();
+        }
+
+        [OneTimeTearDown]
+        public void TestFixtureTearDown()
+        {
+            appHost.Dispose();
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            var service = appHost.Container.Resolve<MyServices>();
+
+            var response = (HelloResponse)service.Any(new Hello { Name = "World" });
+
+            Assert.That(response.Result, Is.EqualTo("Hello, World!"));
+        }
+    }
+}
