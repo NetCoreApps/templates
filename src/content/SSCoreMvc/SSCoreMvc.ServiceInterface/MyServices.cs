6f38aa9691357319784a3fa940a48f5ee3adb682
Darren Reid <darren@solcast.com.au>
2017-06-04 12:49:19 +1000

    Add Mvc template

---
+++ src/content/SSCoreMvc/SSCoreMvc.ServiceInterface/MyServices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ServiceStack;
+using SSCoreMvc.ServiceModel;
+
+namespace SSCoreMvc.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Any(Hello request) =>
+            new HelloResponse { Result = $"Hello, {request.Name}!" };
+    }
+}
