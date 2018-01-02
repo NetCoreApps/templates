82b3fed1ef1f0ed65994524d11017af2036a3139
Darren Reid <darren@solcast.com.au>
2018-01-02 11:44:54 +1100

    Remove files from src, script will regenerate from servicestack/cli. Default install will come directly from NuGet when ready.

--- src/content/angular_lite_spa_template/MyApp/src/common.d.ts
+++
@@ -1,5 +0,0 @@
-﻿//Common Type Definitions for project
-
-// Ensure material-design-lite effects are applied
-// http://stackoverflow.com/a/35895400/85785
-declare var componentHandler;
--- src/content/vue_spa_template/MyApp/src/shared.ts
+++
@@ -1,6 +0,0 @@
-import { JsonServiceClient } from '@servicestack/client';
-
-declare var global; //populated from package.json/karma/globals
-
-export var client = new JsonServiceClient(global.BaseUrl || '/');
-
